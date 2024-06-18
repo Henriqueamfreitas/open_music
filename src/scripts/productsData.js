623853bd0a5e35b915e392a0e79304108b512444
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-06-18 15:38:50 -0300

    ajustes imports

--- src/scripts/productsData.js
+++ src/scripts/productsData.js
@@ -3,7 +3,7 @@ const products = [
     title: 'Magnetite',
     category: 8,
     price: 89.0,
-    img: '../../src/assets/img/1.jpg',
+    img: '../../../open_music/src/assets/img/1.jpg',
     band: 'Scalene',
     year: 2017,
     id: 0,
@@ -12,7 +12,7 @@ const products = [
     title: 'Acabou o Chorare',
     category: 2,
     price: 66.0,
-    img: '../../src/assets/img/2.jpg',
+    img: '../../../open_music/src/assets/img/2.jpg',
     band: 'Novos Baianos',
     year: 1972,
     id: 1,
@@ -21,7 +21,7 @@ const products = [
     title: 'Nirvana Discografia',
     category: 8,
     price: 50.0,
-    img: '../../src/assets/img/3.jpg',
+    img: '../../../open_music/src/assets/img/3.jpg',
     band: 'Nirvava',
     year: 1990,
     id: 2,
@@ -30,7 +30,7 @@ const products = [
     title: 'Both Sides',
     category: 1,
     price: 22.0,
-    img: '../../src/assets/img/4.jpg',
+    img: '../../../open_music/src/assets/img/4.jpg',
     band: 'Phil Collins',
     year: 1993,
     id: 3,
@@ -53,6 +53,6 @@ const categories = [
 ];
 
 const buttonImg = {
-  sunImg: '../../src/assets/img/sun.svg',
-  moonImg: '../../src/assets/img/moon.svg',
+  sunImg: '../../../open_music/src/assets/img/sun.svg',
+  moonImg: '../../../open_music/src/assets/img/moon.svg',
 }
